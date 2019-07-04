f907d3bc1b4bc858b375a470622ff28989a89a08
Ciaran Ainsworth <doctorworm@garappa.chat>
2019-07-04 10:30:36 +0000

    Added new rip command

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 extern crate config;
 extern crate matrix_bot_api;
 extern crate wana_kana;
+extern crate rand;
 
 use matrix_bot_api::{MatrixBot, MessageType};
 use matrix_bot_api::handlers::{Message, StatelessHandler, HandleResult};
 use wana_kana::to_kana::*;
 use wana_kana::to_romaji::*;
+use rand::distributions::{Distribution, Uniform};
 
 fn main() {
 
@@ -58,6 +60,24 @@ fn main() {
 
     bot.add_handler(roma);
 
+// Headpat function
+
+    let mut head = StatelessHandler::new();
+    head.set_cmd_prefix("%");
+    head.register_handle("headpat", headpat);
+
+    bot.add_handler(head);
+
+// Rip function
+
+    let mut rip = StatelessHandler::new();
+    rip.set_cmd_prefix("");
+    rip.register_handle("rip", ripresp);
+    rip.register_handle("rippu", ripresp);
+    rip.register_handle("rup", ripresp);
+
+    bot.add_handler(rip);
+
 // Login function
     
     bot.run(&user, &password, &homeserver_url);
@@ -92,5 +112,20 @@ fn romaconvert(bot: &MatrixBot, message: &Message, _cmd: &str) -> HandleResult {
     let input = &_cmd.trim().to_string();
     let result = to_romaji(input);
     bot.send_message(&format!("{}", result), &message.room, MessageType::TextMessage);
+    HandleResult::StopHandling
+}
+
+fn headpat(bot: &MatrixBot, message: &Message, _cmd: &str) -> HandleResult {
+    bot.send_message("nyan nyan (=^-ω-^=)", &message.room, MessageType::TextMessage);
+    HandleResult::StopHandling
+}
+
+fn ripresp(bot: &MatrixBot, message: &Message, _cmd: &str) -> HandleResult {
+    let mut random = rand::thread_rng();
+    let number = Uniform::from(0.0..1.0);
+    let resp = number.sample(&mut random);
+    if resp > 0.9 {
+        bot.send_message("リップ (´-ω-`)", &message.room, MessageType::TextMessage);
+    }
     HandleResult::StopHandling
 }
